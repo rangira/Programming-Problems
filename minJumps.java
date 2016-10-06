386a95217c7f349d6d64f55a5f812b3c3a19431b
Rudrani Angira <rangira@umail.iu.edu>
2016-10-06 14:48:41 -0400

    Create minJumps.java

---
+++ minJumps.java
@@ -0,0 +1,51 @@
+package presisentialGala;
+
+import java.io.IOException;
+
+public class minJumps {
+
+	public static void main(String[] args) throws IOException 
+	{
+		
+		int arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+		minJumps(arr);
+	}
+	
+	static void minJumps(int[]arr) throws IOException
+	{
+		int i=0,cnt=0,maxSteps=0;
+		for (;i<arr.length;)
+		{
+			maxSteps = arr[i];
+			int j=leap(arr,i,maxSteps);
+			char c = (char) System.in.read();
+			if(j!=0)
+				cnt++;
+			else if(j==0 && (i ==arr.length-1))
+				System.out.println("cnt  = "+cnt);
+			else if (j==0 && i!=arr.length)
+				System.out.println(cnt);
+	
+			i=i+j;
+			if( i >= arr.length-1)
+			{
+				System.out.println(cnt);
+				break;
+			}
+			
+		}
+	}
+	
+	static int leap(int arr[],int i,int maxSteps)
+	{
+		int j =0;
+		for( j=maxSteps;j>=0;j--)
+		{
+			if(i+j < arr.length)
+				break;
+		}
+		
+		return j;
+	}
+
+}
