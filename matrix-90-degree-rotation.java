7e621af217fb03eb7729d6684c715d288693ee0b
Rudrani Angira <rangira@umail.iu.edu>
2016-10-24 22:41:13 -0400

    Create matrix-90-degree-rotation.java

---
+++ matrix-90-degree-rotation.java
@@ -0,0 +1,41 @@
+package pingpong;
+
+public class rotateMat
+{
+	public static void main(String args[])
+	{
+		 int mat[][] =
+			    {
+			        {1, 2, 3, 4},
+			        {5, 6, 7, 8},
+			        {9, 10, 11, 12},
+			        {13, 14, 15, 16}
+			    };
+		 print(rotate(mat));
+		 //print(rotate2(mat));
+	}
+	static int [][] rotate(int [][] input){
+
+		int n =input.length;
+		int m = input[0].length;
+		int [][] output = new int [m][n];
+
+		for (int i=0; i<n; i++)
+			for (int j=0;j<m; j++)
+				//output [j][n-1-i] = input[i][j];//anticlockwise
+				output [n-j-1][i] = input[i][j];//clockwise
+		return output;
+		}
+	
+	
+	static void print(int [][] m)
+	{
+	  for(int i=0;i<m.length;i++){
+		  for(int j=0;j<m[0].length;j++){
+			  System.out.print(m[i][j]+ "  ");
+			  
+		  }
+		  System.out.println();
+	  }
+	}
+}
