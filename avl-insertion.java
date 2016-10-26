06b80ed67fca0425fac8b7e9128e682ae9e34cf0
Rudrani Angira <rangira@umail.iu.edu>
2016-10-26 00:49:46 -0400

    Create avl-insertion.java

---
+++ avl-insertion.java
@@ -0,0 +1,233 @@
+teps to follow for insertion
+Let the newly inserted node be w
+1) Perform standard BST insert for w.
+2) Starting from w, travel up and find the first unbalanced node. Let z be the first unbalanced node, y be the child of z that comes on the path from w to z and x be the grandchild of z that comes on the path from w to z.
+3) Re-balance the tree by performing appropriate rotations on the subtree rooted with z. There can be 4 possible cases that needs to be handled as x, y and z can be arranged in 4 ways. Following are the possible 4 arrangements:
+a) y is left child of z and x is left child of y (Left Left Case)
+b) y is left child of z and x is right child of y (Left Right Case)
+c) y is right child of z and x is right child of y (Right Right Case)
+d) y is right child of z and x is left child of y (Right Left Case)
+
+Following are the operations to be performed in above mentioned 4 cases. In all of the cases, we only need to re-balance the subtree rooted with z and the complete tree becomes balanced as the height of subtree (After appropriate rotations) rooted with z becomes same as it was before insertion. (See this video lecture for proof)
+
+a) Left Left Case
+
+T1, T2, T3 and T4 are subtrees.
+         z                                      y 
+        / \                                   /   \
+       y   T4      Right Rotate (z)          x      z
+      / \          - - - - - - - - ->      /  \    /  \ 
+     x   T3                               T1  T2  T3  T4
+    / \
+  T1   T2
+b) Left Right Case
+
+     z                               z                           x
+    / \                            /   \                        /  \ 
+   y   T4  Left Rotate (y)        x    T4  Right Rotate(z)    y      z
+  / \      - - - - - - - - ->    /  \      - - - - - - - ->  / \    / \
+T1   x                          y    T3                    T1  T2 T3  T4
+    / \                        / \
+  T2   T3                    T1   T2
+c) Right Right Case
+
+  z                                y
+ /  \                            /   \ 
+T1   y     Left Rotate(z)       z      x
+    /  \   - - - - - - - ->    / \    / \
+   T2   x                     T1  T2 T3  T4
+       / \
+     T3  T4
+d) Right Left Case
+
+   z                            z                            x
+  / \                          / \                          /  \ 
+T1   y   Right Rotate (y)    T1   x      Left Rotate(z)   z      y
+    / \  - - - - - - - - ->     /  \   - - - - - - - ->  / \    / \
+   x   T4                      T2   y                  T1  T2  T3  T4
+  / \                              /  \
+T2   T3                           T3   T4
+implementation
+Following is the implementation for AVL Tree Insertion. The following implementation uses the recursive BST insert to insert a new node. In the recursive BST insert, after insertion, we get pointers to all ancestors one by one in bottom up manner. So we don’t need parent pointer to travel up. The recursive code itself travels up and visits all the ancestors of the newly inserted node.
+1) Perform the normal BST insertion.
+2) The current node must be one of the ancestors of the newly inserted node. Update the height of the current node.
+3) Get the balance factor (left subtree height – right subtree height) of the current node.
+4) If balance factor is greater than 1, then the current node is unbalanced and we are either in Left Left case or left Right case. To check whether it is left left case or not, compare the newly inserted key with the key in left subtree root.
+5) If balance factor is less than -1, then the current node is unbalanced and we are either in Right Right case or Right Left case. To check whether it is Right Right case or not, compare the newly inserted key with the key in right subtree root.
+
+CJava
+// Java program for insertion in AVL Tree
+ 
+class Node {
+    int key, height;
+    Node left, right;
+ 
+    Node(int d) {
+        key = d;
+        height = 1;
+    }
+}
+ 
+class AVLTree {
+ 
+    Node root;
+ 
+    // A utility function to get height of the tree
+    int height(Node N) {
+        if (N == null)
+            return 0;
+ 
+        return N.height;
+    }
+ 
+    // A utility function to get maximum of two integers
+    int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+ 
+    // A utility function to right rotate subtree rooted with y
+    // See the diagram given above.
+    Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+ 
+        // Perform rotation
+        x.right = y;
+        y.left = T2;
+ 
+        // Update heights
+        y.height = max(height(y.left), height(y.right)) + 1;
+        x.height = max(height(x.left), height(x.right)) + 1;
+ 
+        // Return new root
+        return x;
+    }
+ 
+    // A utility function to left rotate subtree rooted with x
+    // See the diagram given above.
+    Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+ 
+        // Perform rotation
+        y.left = x;
+        x.right = T2;
+ 
+        //  Update heights
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+ 
+        // Return new root
+        return y;
+    }
+ 
+    // Get Balance factor of node N
+    int getBalance(Node N) {
+        if (N == null)
+            return 0;
+ 
+        return height(N.left) - height(N.right);
+    }
+ 
+    Node insert(Node node, int key) {
+ 
+        /* 1.  Perform the normal BST insertion */
+        if (node == null)
+            return (new Node(key));
+ 
+ 
+        if (key < node.key)
+            node.left = insert(node.left, key);
+        else
+            node.right = insert(node.right, key);
+ 
+        /* 2. Update height of this ancestor node */
+        node.height = 1 + max(height(node.left),
+                              height(node.right));
+ 
+        /* 3. Get the balance factor of this ancestor
+              node to check whether this node became
+              unbalanced */
+        int balance = getBalance(node);
+ 
+        // If this node becomes unbalanced, then there
+        // are 4 cases Left Left Case
+        if (balance > 1 && key < node.left.key)
+            return rightRotate(node);
+ 
+        // Right Right Case
+        if (balance < -1 && key > node.right.key)
+            return leftRotate(node);
+ 
+        // Left Right Case
+        if (balance > 1 && key > node.left.key) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+ 
+        // Right Left Case
+        if (balance < -1 && key < node.right.key) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+ 
+        /* return the (unchanged) node pointer */
+        return node;
+    }
+ 
+    // A utility function to print preorder traversal of the tree.
+    // The function also prints height of every node
+    void preOrder(Node node) {
+        if (node != null) {
+            System.out.print(node.key + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+ 
+    public static void main(String[] args) {
+        AVLTree tree = new AVLTree();
+ 
+        /* Constructing tree given in the above figure */
+        tree.root = tree.insert(tree.root, 10);
+        tree.root = tree.insert(tree.root, 20);
+        tree.root = tree.insert(tree.root, 30);
+        tree.root = tree.insert(tree.root, 40);
+        tree.root = tree.insert(tree.root, 50);
+        tree.root = tree.insert(tree.root, 25);
+ 
+        /* The constructed AVL Tree would be
+             30
+            /  \
+          20   40
+         /  \     \
+        10  25    50
+        */
+        System.out.println("The preorder traversal" +
+                        " of constructed tree is : ");
+        tree.preOrder(tree.root);
+    }
+}
+// This code has been contributed by Mayank Jaiswal
+Run on IDE
+
+Output:
+  Pre order traversal of the constructed AVL tree is
+  30 20 10 25 40 50
+Insertion Examples:
+avlinsert1
+
+avlinsert2-jpg
+
+avlinsert3
+
+avlinsert4
+
+avlinsert5
+
+The above images are taken from here.
+
+Time Complexity: The rotation operations (left and right rotate) take constant time as only few pointers are being changed there. Updating the height and getting the balance factor also take constant time. So the time complexity of AVL insert remains same as BST insert which is O(h) where h is height of the tree. Since AVL tree is balanced, the height is O(Logn). So time complexity of AVL insert is O(Logn).
+
+Comparison with Red Black Tree
+The AVL tree and other self balancing search trees like Red Black are useful to get all basic operations done in O(Log n) time. The AVL trees are more balanced compared to Red Black Trees, but they may cause more rotations during insertion and deletion. So if your application involves many frequent insertions and deletions, then Red Black trees should be preferred. And if the insertions and deletions are less frequent and search is more frequent operation, then AVL tree should be preferred over Red Black Tree.
