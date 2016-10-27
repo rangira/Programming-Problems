c3ef6b82b3368bc211d1a19389c290ece8bd4675
Rudrani Angira <rangira@umail.iu.edu>
2016-10-26 21:15:01 -0400

    Create kth-largest-element.java

---
+++ kth-largest-element.java
@@ -0,0 +1,12 @@
+public int findKthLargest(int[] nums, int k) {
+    PriorityQueue<Integer> q = new PriorityQueue<Integer>(k);
+    for(int i: nums){
+        q.offer(i);
+ 
+        if(q.size()>k){
+            q.poll();
+        }
+    }
+ 
+    return q.peek();
+}
