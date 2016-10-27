b0c33ccd595ecc008eba11a02fc05c258a35642f
Rudrani Angira <rangira@umail.iu.edu>
2016-10-26 21:21:41 -0400

    Create merge-k-sorted-lists.java

---
+++ merge-k-sorted-lists.java
@@ -0,0 +1,35 @@
+The simplest solution is using PriorityQueue. The elements of the priority queue are ordered according to their natural ordering, or by a comparator provided at the construction time (in this case).
+
+Java Solution
+
+public ListNode mergeKLists(ListNode[] lists) {
+    if(lists==null||lists.length==0)
+        return null;
+ 
+    PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>(){
+        public int compare(ListNode l1, ListNode l2){
+            return l1.val - l2.val;
+        }
+    });
+ 
+    ListNode head = new ListNode(0);
+    ListNode p = head;
+ 
+    for(ListNode list: lists){
+        if(list!=null)
+            queue.offer(list);
+    }    
+ 
+    while(!queue.isEmpty()){
+        ListNode n = queue.poll();
+        p.next = n;
+        p=p.next;
+ 
+        if(n.next!=null)
+            queue.offer(n.next);
+    }    
+ 
+    return head.next;
+ 
+}
+Time: log(k) * n.
