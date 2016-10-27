e8e6ec73fe8fa5b8328f2a7d7bb882e38c5dccb2
Rudrani Angira <rangira@umail.iu.edu>
2016-10-27 03:25:04 -0400

    Create lowest-common-ancestor-bst.java

---
+++ lowest-common-ancestor-bst.java
@@ -0,0 +1,24 @@
+/* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+static Node lca(Node root,int v1,int v2)
+    {
+        if (root == null) return null;
+        if (v1 > v2) {          
+            int temp = v2;
+            v2 = v1;
+            v1 = temp;
+        }
+        while (root.data < v1 || root.data > v2) {
+            if (root.data < v1) root = root.right;
+            else if (root.data > v2) root = root.left;
+        }       
+        return root;
+       
+    }
+
