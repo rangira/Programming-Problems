5f227c87917d3301b8628c9d202c2dfb5f64b1bd
Rudrani Angira <rangira@umail.iu.edu>
2017-01-27 01:18:53 -0500

    Update evaluation_script.py

--- evaluation_script.py
+++ evaluation_script.py
@@ -3,6 +3,6 @@ lst = ['amhagen','joeydm','trjroger']
 os.system('ls')
 for ele in lst:
         os.system('mkdir '+ele)
-        os.system('cd '+ele)
+        os.chdir(ele)
         os.system('git clone https://github.iu.edu/'+ele+'/C291-Spring1-2017')
-        os.system('cd ..')
+        os.chdir('..')
